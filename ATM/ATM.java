98d3fd9162ac67c2b8bb5ab14a180183165c023f
Jp120288 <jayaphanindran@gmail.com>
2022-09-26 12:59:45 +0530

    changed

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,14 +1,7 @@
 import java.io.IOException;
 
-public class ATM {
-
-	public static void main(String[] args) throws IOException {
-		OptionMenu optionMenu = new OptionMenu();
-		introduction();
-		optionMenu.mainMenu();
-	}
-
-	public static void introduction() {
-		System.out.println("Welcome to the ATM Project!");
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
